de69852d26f3bd7684a6c946f0e19c112af16137
INB-LAB 1A <technician@lincoln.ac.uk>
2020-01-30 11:44:51 +0000

    Collatz Solution

---
+++ Workshop 2/Workshop 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Workshop_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Collatz collatz = new Collatz(9329);
+        }
+    }
+
+    class Collatz
+    {
+        int positiveInteger = 0;
+        public Collatz(int thePositiveInteger)
+        {
+            if (thePositiveInteger > 0)
+            {
+                positiveInteger = thePositiveInteger;
+            }
+            else
+            {
+                Console.WriteLine("thePositiveInteger must be an integer > 0");
+                return;
+            }
+
+            Console.WriteLine(positiveInteger);
+
+            while (positiveInteger != 1)
+            {
+                if (positiveInteger % 2 == 0)
+                {
+                    positiveInteger = positiveInteger / 2;
+                }
+                else
+                {
+                    positiveInteger = (positiveInteger * 3) + 1;
+                }
+                Console.WriteLine(positiveInteger);
+            }
+        }
+
+  
+
+          
+
+    }
+}
