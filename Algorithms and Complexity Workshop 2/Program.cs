27d364afe7d608d96ccbd86217a439a98a29b2cc
INB-LAB1A <technician@lincoln.ac.uk>
2020-02-03 11:35:28 +0000

    Added complexity of each algorithm to comments above each block of code. Check 3rd functiom afterwards

--- Algorithms and Complexity Workshop 2/Program.cs
+++ Algorithms and Complexity Workshop 2/Program.cs
@@ -7,43 +7,48 @@ namespace Algorithms_and_Complexity_Workshop_2
         static void Main(string[] args)
         {
             var n = 4;
-            //for (int i = 0; i <= (1.5*n) ; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            //for (int i = n; i >=1; i--)
-            //{
-            //    Console.WriteLine(i);
-            //}
 
 
-            //for (int i= 2; i<=n; i++)
+            //
+            //Complexity - O(N) LINEAR 
+            //
+            for (int i = 0; i <= (1.5 * n); i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Complexity = 
+            //for (int i = 2; i <= n; i++)
             //{
-            //    for (int j = 0; j <=n;)
+            //    for (int j = 0; j <= n;)
             //    {
             //        Console.WriteLine(i + ":" + j);
             //        j = j + (n / 4);
             //    }
             //}
 
-            int add_them(int n, int[] A)
-            {
-                var j = 0;
-                for (int i = 1; i <=n; i++)
-                {
-                    j = j + A[i];
-                }
-                var k = 1;
-                for (int i = 1; i <=n; i++)
-                {
-                    k = k + k;
-                }
+            //int add_them(int n, int[] A)
+            //{
+            //    var j = 0;
+            //    for (int i = 1; i <=n; i++)
+            //    {
+            //        j = j + A[i];
+            //    }
+            //    var k = 1;
+            //    for (int i = 1; i <=n; i++)
+            //    {
+            //        k = k + k;
+            //    }
 
-                return j + k;
-            }
+            //    return j + k;
+            //}
 
-            int[] array = {0,2,5,3,7,8};
-            Console.WriteLine(add_them(5, array)); 
+            //int[] array = {0,2,5,3,7,8};
+            //Console.WriteLine(add_them(5, array)); 
         }
     }
 }
